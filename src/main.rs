aff700194de226b69bdc3c17cd2895891172e117
Jakob Lundberg <jakob.lundberg@sofectalabs.io>
2026-06-14 11:48:19 +0200

    Movement speed

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,27 @@
 use macroquad::prelude::*;
 
+const MOVEMENT_SPEED: f32 = 200.0;
+
 #[macroquad::main("My Game")]
 async fn main() {
+    let mut x = screen_width() / 2.0;
+    let mut y = screen_height() / 2.0;
     loop {
+        let delta_time = get_frame_time();
         clear_background(BLUE);
+        if is_key_down(KeyCode::Right) {
+            x += MOVEMENT_SPEED * delta_time;
+        }
+        if is_key_down(KeyCode::Left) {
+            x -= MOVEMENT_SPEED * delta_time;
+        }
+        if is_key_down(KeyCode::Down) {
+            y += MOVEMENT_SPEED * delta_time;
+        }
+        if is_key_down(KeyCode::Up) {
+            y -= MOVEMENT_SPEED * delta_time;
+        }
+        draw_circle(x, y, 16.0, YELLOW);
         next_frame().await;
     }
 }
