82151fde34b394aebf29bb38c092fc8aa2da6b25
Jakob Lundberg <jakob.lundberg@sofectalabs.io>
2026-06-19 07:07:26 +0200

    Menu UI

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use macroquad::audio::{PlaySoundParams, load_sound, play_sound, play_sound_once, stop_sound};
 use macroquad::experimental::animation::{AnimatedSprite, Animation};
 use macroquad::prelude::*;
+use macroquad::ui::{Skin, hash, root_ui};
 use macroquad_particles::{self as particles, AtlasConfig, Emitter, EmitterConfig};
 use std::fs;
 
@@ -141,6 +142,50 @@ async fn main() {
         .expect("Failed to load");
     let sound_explosion = load_sound("explosion.wav").await.expect("Failed to load");
     let sound_laser = load_sound("laser.wav").await.expect("Failed to load");
+
+    let window_background = load_image("window_background.png")
+        .await
+        .expect("Failed to load");
+    let button_background = load_image("button_background.png")
+        .await
+        .expect("Failed to load");
+    let button_clicked_background = load_image("button_clicked_background.png")
+        .await
+        .expect("Failed to load");
+    let font = load_file("atari_games.ttf").await.expect("Failed to load");
+
+    let window_style = root_ui()
+        .style_builder()
+        .background(window_background)
+        .background_margin(RectOffset::new(32.0, 76.0, 44.0, 20.0))
+        .margin(RectOffset::new(0.0, -40.0, 0.0, 0.0))
+        .build();
+    let button_style = root_ui()
+        .style_builder()
+        .background(button_background)
+        .background_clicked(button_clicked_background)
+        .background_margin(RectOffset::new(16.0, 16.0, 16.0, 16.0))
+        .margin(RectOffset::new(16.0, 0.0, -8.0, -8.0))
+        .font(&font)
+        .unwrap()
+        .text_color(WHITE)
+        .font_size(64)
+        .build();
+    let label_style = root_ui()
+        .style_builder()
+        .font(&font)
+        .unwrap()
+        .text_color(WHITE)
+        .font_size(28)
+        .build();
+    let ui_skin = Skin {
+        window_style,
+        button_style,
+        label_style,
+        ..root_ui().default_skin()
+    };
+    root_ui().push_skin(&ui_skin);
+    let window_size = vec2(370.0, 320.0);
 
     let enemy_small_sprite = AnimatedSprite::new(
         17,
@@ -297,19 +342,6 @@ async fn main() {
         draw_background(&game);
         match game.game_state {
             GameState::MainMenu => {
-                if is_key_pressed(KeyCode::Escape) {
-                    std::process::exit(0);
-                }
-                if is_key_pressed(KeyCode::Space) {
-                    game.squares.clear();
-                    game.bullets.clear();
-                    game.explosions.clear();
-                    game.circle.x = screen_width() / 2.0;
-                    game.circle.y = screen_height() / 2.0;
-                    game.score = 0;
-                    game.game_state = GameState::Playing;
-                }
-
                 let text = "Space Blasters";
                 let text_dimensions = measure_text(text, None, 100, 1.0);
                 draw_text(
@@ -320,14 +352,26 @@ async fn main() {
                     WHITE,
                 );
 
-                let text = "Press Space to start";
-                let text_dimensions = measure_text(text, None, 50, 1.0);
-                draw_text(
-                    text,
-                    screen_width() / 2.0 - text_dimensions.width / 2.0,
-                    screen_height() / 2.0,
-                    50.0,
-                    WHITE,
+                root_ui().window(
+                    hash!(),
+                    vec2(screen_width(), screen_height() - window_size.y / 2.0),
+                    window_size,
+                    |ui| {
+                        let (mouse_x, mouse_y) = mouse_position();
+                        ui.label(vec2(80.0, -32.0), "Main Menu");
+                        if ui.button(vec2(65.0, 25.0), "Play") {
+                            game.squares.clear();
+                            game.bullets.clear();
+                            game.explosions.clear();
+                            game.circle.x = screen_width() / 2.0;
+                            game.circle.y = screen_height() / 2.0;
+                            game.score = 0;
+                            game.game_state = GameState::Playing;
+                        }
+                        if ui.button(vec2(65.0, 125.0), "Quit") {
+                            std::process::exit(0);
+                        }
+                    },
                 );
             }
             GameState::Playing => {
